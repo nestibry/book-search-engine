68b8f2bd9130d6213462924ec791c030f0d95f5d
nestibry <nestibry@gmail.com>
2024-02-01 07:26:49 -0600

    updated server /utils/auth.js to match authMiddleware template

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,3 +1,4 @@
+const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
@@ -5,18 +6,24 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-    // function for our authenticated routes
-    authMiddleware: function (req, res, next) {
-        // allows token to be sent via  req.query or headers
-        let token = req.query.token || req.headers.authorization;
+    AuthenticationError: new GraphQLError('Could not authenticate user.', {
+        extensions: {
+            code: 'UNAUTHENTICATED',
+        },
+    }),
 
-        // ["Bearer", "<tokenvalue>"]
+    // function for our authenticated queries/mutations
+    authMiddleware: function ({ req }) {
+        // allows token to be sent via  req.body, req.query or headers
+        let token = req.body.token || req.query.token || req.headers.authorization;
+
+        // "Bearer <tokenvalue>" => ["Bearer", "<tokenvalue>"] => return <tokenvalue>
         if (req.headers.authorization) {
             token = token.split(' ').pop().trim();
         }
 
         if (!token) {
-            return res.status(400).json({ message: 'You have no token!' });
+            return req;
         }
 
         // verify token and get user data out of it
@@ -25,11 +32,10 @@ module.exports = {
             req.user = data;
         } catch {
             console.log('Invalid token');
-            return res.status(400).json({ message: 'invalid token!' });
         }
 
-        // send to next endpoint
-        next();
+        // return the request object so it can be passed to the resolver as `context`
+        return req;
     },
 
     signToken: function ({ username, email, _id }) {
