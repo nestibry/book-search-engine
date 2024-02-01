6a233e554018650b4832ebeaf2505a8954e7bd93
nestibry <nestibry@gmail.com>
2024-02-01 08:52:08 -0600

    init resolvers and typeDefs with simple Query getUsers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,7 +2,25 @@ const { User } = require('../models');
 const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
+    Query: {
+        getUsers: async () => {
+            return User.find();
+        },
+        
+        // getSingleUser
 
+    },
+
+    // Mutation: {
+    //     // createUser
+
+    //     // login
+
+    //     // saveBook
+
+    //     // deleteBook
+
+    // },
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,29 @@
 const typeDefs = `
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        savedBooks: [Book]!
+    }
+
+    type Book {
+        _id: ID
+        authors: [String]!
+        description: String
+        bookId: String
+        image: String
+        title: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        getUsers: [User]
+    }
 
 `;
 
