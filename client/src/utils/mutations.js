8122b5e7da1d73d52f47c19a9fd47d7f6b65ec0c
nestibry <nestibry@gmail.com>
2024-02-01 20:08:11 -0600

    init ADD_USER, SAVE_BOOK, REMOVE_BOOK

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -23,13 +23,61 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-
+    mutation AddUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                email
+                username
+                bookCount
+                savedBooks {
+                    bookId
+                    title
+                    description
+                    authors
+                    image
+                    link
+                }
+            }
+        }
+    }
 `;
 
 export const SAVE_BOOK = gql`
-
+    mutation SaveBook($bookInput: BookInput!) {
+        saveBook(bookInput: $bookInput) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                title
+                authors
+                description
+                image
+                link
+            }
+        }
+    }
 `;
 
 export const REMOVE_BOOK = gql`
-
+    mutation RemoveBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                title
+                description
+                authors
+                image
+                link
+            }
+        }
+    }
 `;
