616474fdc96d916a17f238ef3bddca98a7d14275
nestibry <nestibry@gmail.com>
2024-02-02 05:24:16 -0600

    imported apollo/client dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,22 @@
 import './App.css';
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
-  );
+    return (
+        <>
+            <Navbar />
+            <Outlet />
+        </>
+    );
 }
 
 export default App;
