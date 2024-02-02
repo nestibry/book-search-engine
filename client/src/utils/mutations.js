2798dbc40afc116b0c815c1710e2e532a06f29ae
nestibry <nestibry@gmail.com>
2024-02-01 20:00:04 -0600

    init LOGIN_USER

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,35 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+                bookCount
+                savedBooks {
+                    bookId
+                    title
+                    authors
+                    description
+                    image
+                    link
+                }
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+
+`;
+
+export const SAVE_BOOK = gql`
+
+`;
+
+export const REMOVE_BOOK = gql`
+
+`;
