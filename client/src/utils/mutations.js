ff319ae1e4a4f0acbf37ac4403314722a277b3ce
nestibry <nestibry@gmail.com>
2024-02-03 19:07:37 -0600

    updated mutation to only include necessary data: LOGIN_USER

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -4,21 +4,6 @@ export const LOGIN_USER = gql`
     mutation login($email: String!, $password: String!) {
         login(email: $email, password: $password) {
             token
-            user {
-                _id
-                username
-                email
-                bookCount
-                savedBooks {
-                    _id
-                    bookId
-                    title
-                    authors
-                    description
-                    image
-                    link
-                }
-            }
         }
     }
 `;
