c79f4c7521bd9c4eb01e503a23b934eda5876e13
nestibry <nestibry@gmail.com>
2024-02-10 19:56:17 -0600

    removed saved_books from localStorage in auth.js and also reset login location to homepage upon login

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -35,14 +35,12 @@ class AuthService {
     login(idToken) {
         // Saves user token to localStorage
         localStorage.setItem('id_token', idToken);
-        window.location.assign('/saved');
+        window.location.assign('/');
     }
 
     logout() {
         // Clear user token and profile data from localStorage
         localStorage.removeItem('id_token');
-        // Clear any saved books from localStorage
-        localStorage.removeItem('saved_books');
         // this will reload the page and reset the state of the application
         window.location.assign('/');
     }
