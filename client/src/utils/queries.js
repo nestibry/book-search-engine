32920256079ed2136a8b7d5271a97b2c274e08c1
nestibry <nestibry@gmail.com>
2024-02-10 19:52:58 -0600

    removed QUERY_SAVED_BOOKS

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -20,18 +20,3 @@ export const QUERY_ME = gql`
     }
 `;
 
-export const QUERY_SAVED_BOOKS = gql`
-    query SavedBooks {
-        me {
-            savedBooks {
-                _id
-                bookId
-                title
-                authors
-                description
-                image
-                link
-            }
-        }
-    }
-`;
